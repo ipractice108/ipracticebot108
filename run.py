6f712d446ca542b9c8b056953173d08c40f3775d
Степан Юринов <pdll@Stepans-MacBook-Pro.local>
2020-02-22 03:51:44 +0530

    finally

--- run.py
+++ run.py
@@ -1,22 +1,22 @@
 import os
-import flask
 import constants
+
 from bot import bot
 from telebot import types
+from flask import Flask, request
  
-server = flask.Flask(__name__)
+server = Flask(__name__)
  
 @server.route('/' + constants.token, methods=['POST'])
 def get_message():
     bot.process_new_updates([types.Update.de_json(
-         flask.request.stream.read().decode("utf-8"))])
+         request.stream.read().decode("utf-8"))])
     return "!", 200
  
-@server.route('/')
+@server.route('/', methods=['GET'])
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=constants.heroku_url + constants.token)
-    print("Webhook registered")
     return "Hello from Heroku!", 200
  
 if __name__ == "__main__":
