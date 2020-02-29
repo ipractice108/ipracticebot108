b877e773335c9f165f5eefc9438e83ebbdb5aaf3
ipractice108 <60896049+ipractice108@users.noreply.github.com>
2020-02-29 05:24:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,37 +5,31 @@ import logging
 import telebot
 import constants
 
-bot = telebot.TeleBot(constants.token)
 
-upd = bot.get_updates()
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
+bot = telebot.TeleBot(constants.token)
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
-# upd = bot.get_updates()
-# last_upd = upd[-1]
-# message_from_user = last_upd.message
-# print(message_from_user)
-
 print(bot.get_me())
 
-def log(message, answer):
-  print("\n -----")
-  from datetime import datetime
-  print(datetime.now())
-  print("Сообщение от {} {}, (id = {}) \n Текст - {}".format(message.from_user.first_name,
-                                                                 message.from_user.last_name,
-                                                                 str(message.from_user.id),
-                                                                 message.text))
-  print(answer)
 
-# @bot.message_handler(commands=['help'])
-# def handle_text(message):
-#   bot.send_message(message.chat.id, "Это просто пушка дичайшая")
+def log(message, answer):
+    print("\n -----")
+    from datetime import datetime
+    print(datetime.now())
+    print("Сообщение от {} {}, (id = {}) \n Текст - {}".format(message.from_user.first_name,
+                                                               message.from_user.last_name,
+                                                               str(message.from_user.id),
+                                                               message.text))
+    print(answer)
 
 @bot.message_handler(content_types=['text', 'video'])
 def handle_text(message):
     print("message text is: " + message.text)
     if message.text == "/start":
+        key = telebot.types.ReplyKeyboardMarkup(True, False)
         key.row('зарегистрироваться на вебинар', 'полезные книги')
         key.row('марафон', 'туры')
         send = bot.send_message(message.chat.id, 'Здравствуйте! Вас приветствует чат бот ipractice.club', reply_markup=key)
@@ -53,9 +47,8 @@ def handle_text(message):
                                           '5 видеоуроков для тех, кто регулярно занимается дома 5 лет и возможно думаете о преподавании или уже преподаёте друзьям.\n')
 
     elif message.text == "/beginner":
-        bot.send_message(message.chat.id, 'http://youtu.be/8EgiMc5iwGg')
-        bot.send_message(message.chat.id,
-                         'Поздравляем! Вы получили ознакомительный видео урок и он остается у Вас в доступе!\n'
+        bot.send_message(message.chat.id, 'https://youtu.be/2j3MsZ4E6iQ')
+        bot.send_message(message.chat.id, 'Поздравляем! Вы получили ознакомительный видео урок и он остается у Вас в доступе!\n'
                          '\n'
                          'Хотите начать следующее занятие и получить доступ к полному курсу «новичек» на год, за 3000₽                                                        - нажмите -> /GoBeginner\n'
                          '\n'
@@ -64,17 +57,47 @@ def handle_text(message):
                          'Хотите индивидуальную консультацию или занятие - нажмите -> /individual\n'
                          '\n'
                          'Хотите записаться на физическое групповое занятие в Москве                                                                                         - нажмите -> /group\n')
+        elif message.text == "/GoBeginner":
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="ОПЛАТИТЬ", url="http://ipractice.club/video-course-payment")
+        keyboard.add(url_button)
+        bot.send_message(message.chat.id, "Нажмите на кнопку, что бы перейти на страницу оплаты курса.", reply_markup=keyboard)
 
     elif message.text == "/middle":
         bot.send_message(message.chat.id, 'https://youtu.be/2j3MsZ4E6iQ')
         bot.send_message(message.chat.id, 'Поздравляем! Вы получили ознакомительный видео урок и он остается у Вас в доступе!\n'
                                         '\n'
-                                        'Хотите начать следующее занятие и получить доступ к полному курсу «средний» на год, за 3000₽                                                        - нажмите -> /GoMiddle\n'
+                                        'Хотите начать следующее занятие и получить доступ к полному курсу «средний» на год, за 4000₽                                                        - нажмите -> /GoMiddle\n'
                                         '\n'
                                         'Хотите участвовать в перосональном, недельном онлайн марафоне "Я ВСЕ МОГУ" за 1000₽                                                                - нажмите -> /marathon\n'
                                         '\n'
                                         'Хотите индивидуальную консультацию или занятие - нажмите -> /individual\n'
                                         '\n'
                                         'Хотите записаться на физическое групповое занятие в Москве                                                                                         - нажмите -> /group\n')
+        elif message.text == "/GoMiddle":
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="ОПЛАТИТЬ", url="http://ipractice.club/video-course-payment")
+        keyboard.add(url_button)
+        bot.send_message(message.chat.id, "Нажмите на кнопку, что бы перейти на страницу оплаты курса.", reply_markup=keyboard)
+
+    elif message.text == "/pro":
+        bot.send_message(message.chat.id, 'http://youtu.be/8EgiMc5iwGg')
+        bot.send_message(message.chat.id, 'Поздравляем! Вы получили ознакомительный видео урок и он остается у Вас в доступе!\n'
+                                        '\n'
+                                        'Хотите начать следующее занятие и получить доступ к полному курсу «продвинутый» на год, за 5000₽                                                        - нажмите -> /GoPro\n'
+                                        '\n'
+                                        'Хотите участвовать в перосональном, недельном онлайн марафоне "Я ВСЕ МОГУ" за 1000₽                                                                - нажмите -> /marathon\n'
+                                        '\n'
+                                        'Хотите индивидуальную консультацию или занятие - нажмите -> /individual\n'
+                                        '\n'
+                                        'Хотите записаться на физическое групповое занятие в Москве                                                                                         - нажмите -> /group\n')
+        elif message.text == "/GoPro":
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="ОПЛАТИТЬ", url="http://ipractice.club/video-course-payment")
+        keyboard.add(url_button)
+        bot.send_message(message.chat.id, "Нажмите на кнопку, что бы перейти на страницу оплаты курса.", reply_markup=keyboard)
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
+
+
